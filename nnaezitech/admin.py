374eac2b4b115abe33109bc22623476a64c7319f
Oluwatimileyin Dada <oluwatimileyin962@gmail.com>
2024-01-01 19:05:39 +0100

    edited admin page

--- nnaezitech/admin.py
+++ nnaezitech/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ImageInline(admin.TabularInline):
+    model = Car_Image
+    extra = 1
+
+class CarAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
+    list_display = ["name", "price", "status"]
+    search_fields = ["name", "price", "status"]
+    list_filter = ["name", "price", "status"]
+
+admin.site.register(Car, CarAdmin)
+
+admin.site.register(Message)
+admin.site.register(Company)
