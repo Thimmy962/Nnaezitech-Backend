5c5dcf3347ac705e04db2245d59dea5b6b3773d7
Dada Oluwatimileyin <107270439+Thimmy962@users.noreply.github.com>
2024-01-02 16:20:29 +0100

    Update settings.py

--- Nnaezitech/settings.py
+++ Nnaezitech/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/car_images/')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
