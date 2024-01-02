c382559f462f8ec4e66459f878a146ea04c7e2f2
Oluwatimileyin Dada <oluwatimileyin962@gmail.com>
2024-01-02 13:42:27 +0100

    collect static

--- Nnaezitech/settings.py
+++ Nnaezitech/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,19 +9,17 @@ SECRET_KEY = 'django-insecure-ba70!f1_)lwkrt$od-^19hkrc@v0s+22zb07zlo!pvh%dnok!5
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = os.environ.get("DEBUG", 'False').lower()  == 'true'
-# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = [
-
-]
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 CORS_ALLOWED_ORIGINS = [
     "https://nnaezifrontend.onrender.com",
 ]
 
-mimetypes.add_type("text/css", ".css", True)
-
 CORS_ALLOW_CREDENTIALS = True
+
+# Application definition
+
 
 
 INSTALLED_APPS = [
@@ -79,8 +76,8 @@ DATABASES = {
     }
 }
 
-# database_url = os.environ.get('DATABASE_URL')
-# DATABASES['default'] = parse(database_url)
+database_url = os.environ.get('DATABASE_URL')
+DATABASES['default'] = parse(database_url)
 
 
 # Password validation
