482b3c48e8e01e28a8ca2594ddabf73a0bb739af
Oluwatimileyin Dada <oluwatimileyin962@gmail.com>
2024-01-25 23:30:58 +0100

    new database

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ COPY ./requirements.txt /app/
 
 RUN pip install -r requirements.txt
 
+
 COPY . /app
 
 ENTRYPOINT [ "gunicorn", "core.wsgi" ]
--- Nnaezitech/settings.py
+++ Nnaezitech/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-ba70!f1_)lwkrt$od-^19hkrc@v0s+22zb07zlo!pvh%dnok!5
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = os.environ.get("DEBUG", 'False').lower()  == 'true'
+DEBUG = os.environ.get("DEBUG", 'False').lower()  == 'true'
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 CORS_ALLOWED_ORIGINS = os.environ.get("ALLOWED_ORIGINS").split(" ")
 SECRET_KEY = os.environ.get("SECRET_KEY")
